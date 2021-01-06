d42aced5a2fd2e498b64f7e9ea46490b4a2a7f00
William <jiayewang65@gmail.com>
2021-01-06 13:25:45 -0600

    Create 0309.Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp

---
+++ DP/0309.Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+      int n = p.size();
+      if (n < 2)
+        return 0;
+      int hold = INT_MIN, sold = 0, cooled = 0;
+      for (int i = 0; i < n; i++) {
+        int h = hold, s = sold, c = cooled;
+        hold = max(h, c - p[i]);
+        sold = h + p[i];
+        cooled = max(c, s);
+      }
+      return max(sold, cooled);
+    }
+};
