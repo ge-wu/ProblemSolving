77865470c2f12ed81f27aeda409fb44646012459
William <jiayewang65@gmail.com>
2021-01-18 16:37:35 -0600

    Update 0714.Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp

--- DP/0714.Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp
+++ DP/0714.Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp
@@ -4,7 +4,7 @@ public:
         int hold = -50001, sold = 0;
         for (int i = 0; i < prices.size(); i++) {
             int h = hold, s = sold;
-            hold = max(h, sold-prices[i]);
+            hold = max(h, sold - prices[i]);
             sold = max(h + prices[i] - fee, s);
         }
         return max(hold, sold);
