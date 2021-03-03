d93dec26a637f82474dd6d52f53bdb607074358f
Jiaye William Wang <jiayewang65@gmail.com>
2021-03-03 14:27:38 -0600

    Update 0198.House_Robber.cpp

--- DP/0198.House_Robber.cpp
+++ DP/0198.House_Robber.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (!n) 
+        if (!nums.size()) {
             return 0;
-        
-        vector<vector<int>>dp(n, {0, 0});
-        dp[0][0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            dp[i][0] += nums[i] + dp[i - 1][1];
-            dp[i][1] += max(dp[i - 1][0], dp[i - 1][1]);
         }
-        return max(dp[n - 1][0], dp[n - 1][1]);
+        
+        int doIt = nums[0], dontDoIt = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            int a = doIt, b = dontDoIt;
+            doIt = b + nums[i];
+            dontDoIt = max(a, b);
+        }
+        return max(doIt, dontDoIt);
     }
 };
