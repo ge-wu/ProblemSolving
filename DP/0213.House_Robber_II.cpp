c87d13c75e95085acdf9fb5039d3dafa6c5653df
Jiaye William Wang <jiayewang65@gmail.com>
2021-03-05 18:33:45 -0600

    Update 0213.House_Robber_II.cpp

--- DP/0213.House_Robber_II.cpp
+++ DP/0213.House_Robber_II.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (!n) return 0;
-        if (n == 1) return nums[0];
-        
-        int doIt = nums[1], dontDoIt = 0;
-        int ans1 = 0, ans2 = 0;
-        
-        for (int i = 2; i < n; i++) {
-            int temp1 = doIt, temp2 = dontDoIt;
-            doIt = temp2 + nums[i];
-            dontDoIt = max(temp1, temp2);
+        if (nums.size() == 1) {
+            return nums[0];
         }
-        ans1 = max(doIt, dontDoIt);
+        int ans1, ans2;
+        int rob, noRob;
         
-        doIt = nums[0];
-        dontDoIt = 0;
-        for (int i = 1; i < n - 1; i++) {
-            int temp1 = doIt, temp2 = dontDoIt;
-            doIt = temp2 + nums[i];
-            dontDoIt = max(temp1, temp2);
+        rob = nums[0];
+        noRob = 0;
+        for (int i = 1; i < nums.size() - 1; i++) {
+            int a = rob, b = noRob;
+            rob = nums[i] + b;
+            noRob = max(a, b);
         }
-        ans2 = max(doIt, dontDoIt);
-        return max(ans1, ans2);
+        ans1 = max(rob, noRob);
+        
+        rob = nums[1];
+        noRob = 0;
+        for (int i = 2; i < nums.size(); i++) {
+            int a = rob, b = noRob;
+            rob = nums[i] + b;
+            noRob = max(a, b);
+        }
+        return max(ans1, max(rob, noRob));
     }
 };
