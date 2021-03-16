18f80f6fb623387fa09c708d1d6103636e143ff2
Jiaye William Wang <jiayewang65@gmail.com>
2021-03-16 11:16:43 -0600

    Update 0714.Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp

--- DP/0714.Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp
+++ DP/0714.Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int hold = -50001, sold = 0;
-        for (int i = 0; i < prices.size(); i++) {
+        int n = prices.size();
+        int hold = -10000000, sold = 0;
+        for (int i = 0; i < n; i++) {
             int h = hold, s = sold;
             hold = max(h, sold - prices[i]);
             sold = max(h + prices[i] - fee, s);
         }
-        return max(hold, sold);
+        return sold;
     }
 };
