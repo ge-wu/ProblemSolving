a357987a53a6000c59d87b96305e9371c2c41bee
Jiaye William Wang <jiayewang65@gmail.com>
2021-06-03 18:21:01 -0600

    Create 0515.Find_Largest_Value_in_Each_Tree_Row.cpp

---
+++ Tree/0515.Find_Largest_Value_in_Each_Tree_Row.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    
+    vector<int> largestValues(TreeNode* root) {
+        helper(root, 0);
+        return ans;
+    }
+    
+    void helper(TreeNode *root, int depth) {
+        if (!root)
+            return;
+        if (depth < ans.size()) {
+            ans[depth] = max(ans[depth], root->val);
+        } else {
+            ans.push_back(root->val);
+        }
+        helper(root->left, depth + 1);
+        helper(root->right, depth + 1);
+    }
+};
