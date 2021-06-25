e3e574b6d8fa97694341dfce48e5fe0d658692b5
Jiaye William Wang <jiayewang65@gmail.com>
2021-06-24 18:42:36 -0600

    Update 0576.Out_of_Boundary_Paths.cpp

--- DynamicProgramming/0576.Out_of_Boundary_Paths.cpp
+++ DynamicProgramming/0576.Out_of_Boundary_Paths.cpp
@@ -1,22 +1,27 @@
-#define ll long long
-
 class Solution {
 public:
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        vector<vector<ll>> arr(m, vector<ll>(n, 0));
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         while (maxMove--) {
-            vector<vector<ll>> w = arr;
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    ll up = i - 1 < 0 ? 1 : arr[i - 1][j];
-                    ll down = i + 1 >= m ? 1 : arr[i + 1][j];
-                    ll right = j + 1 >= n ? 1 : arr[i][j + 1];
-                    ll left = j - 1 < 0 ? 1 : arr[i][j - 1];
-                    w[i][j] = (up + down + right + left) % 1000000007;
+            auto copy = dp;
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    int total = 0;
+                    for (auto d: dirs) {
+                        int x = i + d.first, y = j + d.second;
+                        if (x < 0 || y < 0 || x >= m || y >= n) {
+                            ++total;
+                        } else {
+                            total += dp[x][y];
+                        }
+                        total %= 1000000007;
+                    }
+                    copy[i][j] = total;
                 }
             }
-            arr = w;
+            dp = copy;
         }
-        return arr[startRow][startColumn];
+        return dp[startRow][startColumn];
     }
 };
