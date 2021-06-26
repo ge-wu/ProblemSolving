93dcd96d5662a4887a09a5e3553964d427907311
Jiaye William Wang <jiayewang65@gmail.com>
2021-06-25 18:51:05 -0600

    Update 0120.Triangle.cpp

--- DynamicProgramming/0120.Triangle.cpp
+++ DynamicProgramming/0120.Triangle.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        if (n == 1) return triangle[0][0];
-        
+    int minimumTotal(vector<vector<int>>& A) {
+        int n = A.size();
         vector<vector<int>> dp(n);
-        dp[0] = triangle[0];
-        
-        for (int i = 1; i < n; i++) {
-            dp[i] = vector<int>(triangle[i].size(), 0);
-            for (int j = 0; j < triangle[i].size(); j++) {
-                dp[i][j] = triangle[i][j];
-                if (!j)
+        dp[0] = A[0];
+        for (int i = 1; i < n; ++i) {
+            dp[i] = vector<int>(A[i].size(), 0);
+            for (int j = 0; j < A[i].size(); ++j) {
+                dp[i][j] = A[i][j];
+                if (j == 0) {
                     dp[i][j] += dp[i - 1][0];
-                else if (j == triangle[i].size() - 1) 
-                    dp[i][j] += dp[i - 1][j - 1];
-                else 
+                } else if (j == dp[i].size() - 1) {
+                    dp[i][j] += dp[i - 1].back();
+                } else {
                     dp[i][j] += min(dp[i - 1][j - 1], dp[i - 1][j]);
+                }
             }
         }
-        return *min_element(dp[n - 1].begin(), dp[n - 1].end());
+        return *min_element(dp.back().begin(), dp.back().end());
     }
 };
