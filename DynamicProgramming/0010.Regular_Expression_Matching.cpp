667f5653879c76dfce3160b3e6ae1d789871eb12
Jiaye William Wang <jiayewang65@gmail.com>
2021-07-12 19:56:47 -0600

    Update 0010.Regular_Expression_Matching.cpp

--- DynamicProgramming/0010.Regular_Expression_Matching.cpp
+++ DynamicProgramming/0010.Regular_Expression_Matching.cpp
@@ -4,23 +4,24 @@ public:
         int m = s.size(), n = p.size();
         s = "#" + s;
         p = "#" + p;
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        int dp[50][50];
+        memset(dp, 0, sizeof(dp));
         dp[0][0] = 1;
-        for (int i = 2; i <= n; i++) {
-            if (p[i] == '*')
-                dp[0][i] = dp[0][i - 2];
+        for (int i = 2; i <= n; ++i) {
+            if (p[i] == '*') {
+                dp[0][i] = dp[0][i-2];
+            }
         }
-        
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
                 if (s[i] == p[j] || p[j] == '.') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (p[j] == '*') {
-                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s[i] == p[j -1] || p[j - 1] == '.'));
+                    dp[i][j] = dp[i-1][j-1];
+                } 
+                if (p[j] == '*') {
+                    dp[i][j] = dp[i][j-2] || (dp[i-1][j] && (s[i] == p[j-1] || p[j-1] == '.'));
                 }
             }
         }
-        
         return dp[m][n];
     }
 };
