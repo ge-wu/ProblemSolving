f3ee067b35d495b498ee91bcd20c58c58c0ff777
Jiaye William Wang <jiayewang65@gmail.com>
2021-07-14 16:26:45 -0600

    Update 0791.Custom_Sort_String.cpp

--- String/0791.Custom_Sort_String.cpp
+++ String/0791.Custom_Sort_String.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     string customSortString(string order, string str) {
-        string ans = "";
-        vector<int> cnt(26, 0);
+        int cnt[26] = {};
         for (char c: str) {
-            ++cnt[c - 'a'];
+            cnt[c - 'a']++;
         }
+        string ans = "";
         for (char c: order) {
             string s(cnt[c - 'a'], c);
             cnt[c - 'a'] = 0;
             ans += s;
         }
         for (int i = 0; i < 26; ++i) {
-            char c = i + 'a';
-            ans += string(cnt[i], c);
+            string s(cnt[i], char(i + 'a'));
+            ans += s;
         }
         return ans;
     }
